7930239b0e714e317ca43edb478336466037ec7b
RAHUL <rahulmulik9@gmail.com>
2024-11-06 16:42:27 +0530

    Added customer entity Added customer repository Added API to register user.

---
+++ src/main/java/com/rahul/spring_security/repository/CustomerRepository.java
@@ -0,0 +1,13 @@
+package com.rahul.spring_security.repository;
+import com.rahul.spring_security.model.Customer;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface CustomerRepository extends CrudRepository<Customer,Long> {
+
+    List<Customer> findByEmail(String email);
+    
+}
